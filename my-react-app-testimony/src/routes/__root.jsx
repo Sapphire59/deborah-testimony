55342037766ec2c5f5cbe553a0f5df1c3877babb
Deborah <liz.joy43@gmail.com>
2025-06-18 15:19:33 -0600

    fourth commit

--- my-react-app-testimony/src/routes/__root.jsx
+++ my-react-app-testimony/src/routes/__root.jsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2 bg-gray-100 border-2 border-rose-300 rounded-md">
+      <div className="p-2 flex gap-2 bg-stone-300 border-2 border-rose-300 rounded-md">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
--- my-react-app-testimony/src/routes/index.jsx
+++ my-react-app-testimony/src/routes/index.jsx
@@ -7,27 +7,25 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <div class="container mx-auto px-3 ">
-      <div className="p-2 text-left text-md/5 leading-6">
-        <div class="grid grid-cols-2 gap-6 mt-6 p-5">
+      <div className="p-2 text-left text-md/6 leading-6">
+        <div class="grid grid-cols-2 gap-3 mt-4">
           <div>
-            <p>
-              Dear Friend, I built this site to share with you my testimony of
-              the amazing healing power of my Lord Jesus Christ. I suffered with
+            <p class="border-1 m-4 p-2 bg-white rounded-md shadow-2xl">
+              Dear Friend,
+              <br />I built this site to share with you my testimony of the
+              amazing healing power of my Lord Jesus Christ. I suffered with
               terrible, painful eczema for many years, and I had breathing
               problems on and off for years as well. But praise God He has
-              healed me. I also believe He has renewed my youth to the point I
-              don't look my age.
+              healed me.
             </p>
           </div>
           <div>
-            <p>
+            <p class="border-1 m-4 p-4 bg-white rounded-md shadow-2xl">
               Healing is one of many precious gifts Jesus has purchased for us
-              on the Cross. It is His will for you to walk in healing & health,
-              pain and sickness is a curse that Christ has redeemed us from.
-              Jesus loves you so much and its His great desire to heal you. I
-              pray this site will inspire you to look into the Word of God and
-              let the Holy Spirit reveal to you His wisdom on health and healing
-              for you personally.
+              on the Cross. Jesus loves you so much and its His great desire to
+              heal you. I pray this site will inspire you to look into the Word
+              of God and let the Holy Spirit reveal to you His wisdom on health
+              and healing for you personally.
             </p>
           </div>
         </div>
